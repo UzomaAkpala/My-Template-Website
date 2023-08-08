2a6ef075ea6c00f4a8479e43e35d248d89d957f6
UzomaAkpala <francisakpala@yahoo.com>
2023-08-08 22:23:37 +0100

    Landing Page and Error page Update

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,30 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/LandingPage";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not-found" />
+          <h3>Ohh! page not found</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to="/dashboard">Back to home page</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   console.log(error);
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">Back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 };
 export default Error;
