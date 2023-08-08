a00e2b1d33534a0a0fd579c965e5bfadf06e45ef
UzomaAkpala <francisakpala@yahoo.com>
2023-08-08 14:07:14 +0100

    React-router updates

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+{
+  /* Remeber that the Link tag is only for pages within the project, when you want to link to an external page like google you still have to use the original <a> tags */
+}
+
+const Login = () => {
+  <div>
+    return <h1>Login Page</h1>;<Link to="/register">Register Page</Link>;
+  </div>;
+};
+export default Login;
